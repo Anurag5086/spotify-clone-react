8bfbcd95901f2cdf8db413da3255429d2ad523c6
Anurag Gupta <anuraggupta5086@gmail.com>
2020-09-04 16:17:59 +0400

    Login API Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Components/Login'
+import { getTokenFromUrl } from './Components/spotify'
+import SpotifyWebApi from 'spotify-web-api-js'
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+      const hash = getTokenFromUrl();
+      window.location.hash = "";
+
+      let _token = hash.access_token;
+
+      if(_token){
+        setToken(_token);
+
+        spotify.setAccessToken(_token);
+        spotify.getMe().then(user =>{
+          console.log('🙍‍♂️' , user);
+        })
+      }
+
+  }, []); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        token? (
+          <h1>Helloo!</h1>
+          // <Player /> 
+          // START BUILDING PLAYER COMPONENT
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Login.css'
+import { loginUrl } from './spotify'
+
+
+
+function Login() {
+    return (
+        <div className="login">
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
+        <h2 className="head__text">Sign up for free to start listening.</h2>
+            <a href={loginUrl} className="login__button">LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login
