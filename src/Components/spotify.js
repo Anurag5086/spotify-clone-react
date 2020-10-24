7916d058ddd80428dcbce39cfa189583435b49b7
Anurag5086 <anuraggupta5086@gmail.com>
2020-10-24 14:32:15 +0530

    firebase deploy

--- src/Components/spotify.js
+++ src/Components/spotify.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://spotify-clone-react-195bc.web.app";
 const clientId = "22c27d02d79542f7a29fe2de59651108";
 
 const scopes = [
